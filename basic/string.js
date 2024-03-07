277538a3c3cb43129e80f60f0de47ba3264ed715
Sangam Kumar <134846331+sangamkumar123@users.noreply.github.com>
2024-03-07 19:24:49 +0000

    about string

---
+++ basic/string.js
@@ -0,0 +1,16 @@
+// const name = "sangam kumar"
+// const rollNo = 150
+
+// console.log(name +" " +  rollNo);
+
+// console.log(`My name is ${name} and my roll no. is ${rollNo}`)
+
+const myName = new String("Mr sangam kumar")
+// console.log(myName)
+
+// console.log(myName.charAt(5));
+// console.log(myName.indexOf('a'));
+// console.log(myName.split(' '));
+console.log(myName.search('san'));
+console.log(myName.replace('san', 'SAN'));
+console.log(myName.toUpperCase);
