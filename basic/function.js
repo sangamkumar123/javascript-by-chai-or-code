9aa893dac39d099ebdfe1abacd7f75b3c807a248
Sangam Kumar <134846331+sangamkumar123@users.noreply.github.com>
2024-03-10 10:57:26 +0000

    basic about function

---
+++ basic/function.js
@@ -0,0 +1,29 @@
+const arr = [1,3,2,5,7,8,6]
+
+function print_2_value(myarray){
+    return myarray[1]
+}
+
+// console.log(`value of 2nd index is ` + print_2_value(arr))
+// console.log(`value of 2nd index is  ${ print_2_value(arr)} `)
+
+
+const myObject = {
+
+    name : "sangam",
+    age : 21,
+    roll_no : 150
+}
+
+function pass_object(Any_Object){
+    console.log(Any_Object);
+}
+// pass_object(myObject)
+
+pass_object(
+    
+    {name : "sangam",
+    age : 21,
+    roll_no : 150
+    }
+)
